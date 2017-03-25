6b0351e5cd68fd0d641e85ee964b175a3c154664
Lou <DangerousElectrician@gmail.com>
2017-03-24 19:52:38 -0500

    don't need this

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #define VESC_ID 0
 #define CAN_INTERFACE "can0"
 
-int main(int argc, char **argv) {
+int main() { //int argc, char **argv) {
 	Vesc motor((char*) "can0", VESC_ID); //TODO: this char* thing is a bit iffy, is this the right way to do it
 	motor.setCustom(0.1);
 	//float current = 0;
