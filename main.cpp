f0fdde595598064d152db3e82134357f022f51ad
Lou <DangerousElectrician@gmail.com>
2017-04-17 20:29:27 -0500

    testing encoder index detect

--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
 #include "VescSocketCAN.h"
 #include <iomanip>
+#include <stdlib.h>
 #define VESC_ID 0
 #define CAN_INTERFACE "can0"
 
-int main() { //int argc, char **argv) {
-	Vesc motor((char*) "can0", VESC_ID); //TODO: this char* thing is a bit iffy, is this the right way to do it
-	motor.setCustom(0.1);
+int main( int argc, char **argv) {
+	if(argc < 3) {
+		std::cout << "remember to provide vesc id" << std::endl;
+		return 0;
+	}
+	Vesc motor((char*) "can0", atoi(argv[1]), atoi(argv[2])); //TODO: this char* thing is a bit iffy, is this the right way to do it
+	//motor.setCustom(0.1);
 	//float current = 0;
 	while(1) {
-		std::cout << std::setprecision(4) << "mcur " << motor.getCurrent() << std::setw(9) << "\tduty " <<  motor.getDutyCycle() << std::setw(9) << "\trpm " << motor.getRpm() << std::setw(9) << "\tpos " <<motor.getPosition() << std::setw(9) << "\ttach " << motor.getTachometer() << std::setw(9) << "\twatth " << motor.getWattHours() << std::setw(9) << "\tincur " << motor.getInCurrent() << std::setw(9) << "\tVin " << motor.getVin() << std::setw(9) << "\ttempM " << motor.getTempMotor() << std::setw(9) << "\ttempP " << motor.getTempPCB() << std::setw(9) << "\tfault  " << motor.getFaultCode() << std::setw(9) << "\tstate " << motor.getState() <<  "\talive " << motor.isAlive() << std::endl;
+		std::cout << std::setprecision(4) << "mcur " << motor.getCurrent() << std::setw(9) << "\tduty " <<  motor.getDutyCycle() << std::setw(9) << "\trpm " << motor.getRpm() << std::setw(9) << "\tpos " <<motor.getPosition() << std::setw(9) << "\ttach " << motor.getTachometer() << std::setw(9) << "\twatth " << motor.getWattHours() << std::setw(9) << "\tincur " << motor.getInCurrent() << std::setw(9) << "\tVin " << motor.getVin() << std::setw(9) << "\ttempM " << motor.getTempMotor() << std::setw(9) << "\ttempP " << motor.getTempPCB() << std::setw(9) << "\tfault  " << motor.getFaultCode() << std::setw(9) << "\tstate " << motor.getState() << "\tencind " << motor.encoderIndexFound() <<  "\talive " << motor.isAlive() << std::endl;
 		//motor.getCurrent();
 		//motor.setCurrent(current);
 		//current = current + 0.01;
